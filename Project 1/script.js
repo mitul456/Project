6deb8d6bb1685f2df842e705836780b3e52ec551
mitul456 <100792680+mitul456@users.noreply.github.com>
2024-02-29 22:46:37 +0600

    update

---
+++ Project 1/script.js
@@ -0,0 +1,65 @@
+
+function showMenu() {
+    const menuShow = document.querySelector(".sidebar");
+    if (menuShow.style.display === "flex") {
+        menuShow.style.display = "none";
+    } else {
+        menuShow.style.display = "flex";
+    }
+}
+
+function hideMenu() {
+    const menuShow = document.querySelector(".sidebar");
+    if (menuShow.style.display === "flex") {
+        menuShow.style.display = "none";
+    } else {
+        menuShow.style.display = "flex";
+    }
+}
+
+
+
+
+
+
+
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+
+
+// ============================
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
